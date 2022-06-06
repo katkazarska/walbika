9133fed8dbb7997b52770a962ba6a6716d3f29ae
katkazarska <62121596+katkazarska@users.noreply.github.com>
2022-06-06 11:36:47 +0200

    Update walbika.js

--- website/walbika.js
+++ website/walbika.js
@@ -6,4 +6,5 @@ var folder = "images/";
 function choosePic() {
     var randomNum = Math.floor(Math.random() * imageCount);
     document.getElementById('myPicture').src = folder + randomNum.toString() + ".jpg";
+    document.getElementById('myPicture').alt = folder + randomNum.toString() + ".jpg";
 }
